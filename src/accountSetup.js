4e824127bb61be92ae286aa131fadcc839830f06
Tyler Kiesman <tylerkiesman@gmail.com>
2020-10-30 00:12:47 -0400

    Fixed issue where couldn't get sign up working

--- src/accountSetup.js
+++ src/accountSetup.js
@@ -1,47 +1,56 @@
 import React from 'react';
-import {addNewUser, getChecksLiked} from "./dataInterface";
-import {Email} from "@material-ui/icons";
-import * as db from "./systemData";
+import * as data from './dataInterface';
 
 class AccountSetup extends React.Component{
+    constructor(props){
+        super(props);
+        this.addNewUser = this.addNewUser.bind(this);
+    }
 
     getChecksLiked = () => {
-        let checks = document.getElementsByClassName('form-check-inline1');
+        let checks = document.getElementsByClassName('likeBox');
+        if(checks.length == 0){
+            return []
+        }
         let arry = [];
         for (let i = 0; i < 21; i++) {
-            if (checks[i].checked === true) {
-                arry += checks[i].value;
+            if(checks[i] != null){
+                if (checks[i].checked === true){
+                    arry.push(checks[i].value);
+                }
             }
         }
         return arry
     }
 
     getChecksDisliked = () => {
-        let checks = document.getElementsByClassName('form-check-inline');
+        let checks = document.getElementsByClassName('dislikeBox');
+        if(checks.length == 0){
+            return []
+        }
         let arry = [];
         for(let i = 0; i < 21; i++) {
-            if (checks[i].checked === true){
-                arry += checks[i].value;
+            if(checks[i] != null){
+                if (checks[i].checked === true){
+                    arry.push(checks[i].value);
+                }
             }
         }
         return arry
     }
 
-    addNewUser = (email, name, password, likedGenres, dislikedGenres, likedMovies, dislikedMovies) => {
-        db.emailsToInfo[email] = {
-            "lists": [],
-            "name": name,
-            "password": password,
-            "likedGenres": [],
-            "dislikedGenres": [],
-            "likedMovies": likedMovies,
-            "dislikedMovies": dislikedMovies,
-            "friends": [],
-            "history": [],
-            "guestCode": "code",
-            "email": email
+    addNewUser = (e) => {
+        const likedGenres = this.getChecksLiked();
+        const dislikedGenres = this.getChecksDisliked();
+        const email = document.getElementById("Email").value
+        const name = document.getElementById("Username").value
+        const password = document.getElementById("Password").value
+        const likedMovies = document.getElementById("Liked_Movies").value
+        const dislikedMovies = document.getElementById("Disliked_Movies").value
+        data.addNewUser(email,name,password,likedGenres,dislikedGenres,likedMovies,dislikedMovies);
+        console.log(data.getUserInfo(email))
     }
-}
+
     render(){ 
         return (
             <body style={{backgroundColor: "#262626"}}>
@@ -68,32 +77,32 @@ class AccountSetup extends React.Component{
                 <div class="checkbox bg-#262626 text-white col-xs-4">
                     <div class="control-group">
                         <p>Movie genres you like:<br/></p>
-                        <div class="form-check-inline1">
-                            <label class="form-check-inline1"><input type="checkbox" value="Action"/>Action</label>
-                            <label class="form-check-inline1"><input type="checkbox" value="Adventure"/>Adventure</label>
-                            <label class="form-check-inline1"><input type="checkbox" value="Comedy"/>Comedy</label>
-                            <label class="form-check-inline1"><input type="checkbox" value="Crime"/>Crime</label>
-                            <label class="form-check-inline1"><input type="checkbox" value="Drama"/>Drama</label>
-                            <label class="form-check-inline1"><input type="checkbox" value="Fantasy"/>Fantasy</label>
-                            <label class="form-check-inline1"><input type="checkbox" value="Historical"/>Historical</label>
+                        <div class="form-check-inline">
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Action"/>Action</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Adventure"/>Adventure</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Comedy"/>Comedy</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Crime"/>Crime</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Drama"/>Drama</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Fantasy"/>Fantasy</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Historical"/>Historical</label>
                         </div>
                         <div class="form-check-inline">
-                            <label class="form-check-inline"><input type="checkbox" value="Horror"/>Horror</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Magical_Realism"/>Magical Realism</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Mystery"/>Mystery</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Philosophical"/>Philosophical</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Political"/>Political</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Romance"/>Romance</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Saga"/>Saga</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Horror"/>Horror</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Magical_Realism"/>Magical Realism</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Mystery"/>Mystery</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Philosophical"/>Philosophical</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Political"/>Political</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Romance"/>Romance</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Saga"/>Saga</label>
                         </div>
                         <div class="form-check-inline">
-                            <label class="form-check-inline"><input type="checkbox" value="Satire"/>Satire</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Science_Fiction"/>Science Fiction</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Social"/>Social</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Speculative"/>Speculative</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Thriller"/>Thriller</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Urban"/>Urban</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Western"/>Western</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Satire"/>Satire</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Science_Fiction"/>Science Fiction</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Social"/>Social</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Speculative"/>Speculative</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Thriller"/>Thriller</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Urban"/>Urban</label>
+                            <label class="form-check-inline"><input className="likeBox" type="checkbox" value="Western"/>Western</label>
                         </div>
                     </div>
                 </div>
@@ -103,31 +112,31 @@ class AccountSetup extends React.Component{
                     <div class="control-group">
                         <p>Movie genres you dislike:<br/></p>
                         <div class="form-check-inline">
-                            <label class="form-check-inline"><input type="checkbox" value="Action"/>Action</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Adventure"/>Adventure</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Comedy"/>Comedy</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Crime"/>Crime</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Drama"/>Drama</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Fantasy"/>Fantasy</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Historical"/>Historical</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Action"/>Action</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Adventure"/>Adventure</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Comedy"/>Comedy</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Crime"/>Crime</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Drama"/>Drama</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Fantasy"/>Fantasy</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Historical"/>Historical</label>
                         </div>
                         <div class="form-check-inline">
-                            <label class="form-check-inline"><input type="checkbox" value="Horror"/>Horror</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Magical_Realism"/>Magical Realism</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Mystery"/>Mystery</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Philosophical"/>Philosophical</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Political"/>Political</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Romance"/>Romance</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Saga"/>Saga</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Horror"/>Horror</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Magical_Realism"/>Magical Realism</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Mystery"/>Mystery</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Philosophical"/>Philosophical</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Political"/>Political</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Romance"/>Romance</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Saga"/>Saga</label>
                         </div>
                         <div class="form-check-inline">
-                            <label class="form-check-inline"><input type="checkbox" value="Satire"/>Satire</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Science_Fiction"/>Science Fiction</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Social"/>Social</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Speculative"/>Speculative</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Thriller"/>Thriller</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Urban"/>Urban</label>
-                            <label class="form-check-inline"><input type="checkbox" value="Western"/>Western</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Satire"/>Satire</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Science_Fiction"/>Science Fiction</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Social"/>Social</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Speculative"/>Speculative</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Thriller"/>Thriller</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Urban"/>Urban</label>
+                            <label class="form-check-inline"><input className="dislikeBox" type="checkbox" value="Western"/>Western</label>
                         </div>
                     </div>
                 </div>
@@ -140,15 +149,8 @@ class AccountSetup extends React.Component{
                 <label for="Disliked_Movies">Movies you like:</label>
                 <input type="text" style={{backgroundColor: "#262626", color: "white"}} class="form-control bg-#262626 col-sm-4" id="Disliked_Movies"/>
             </div>
-            <button class="btn btn-sm btn-block text-white text-center  btn-outline-light col-sm-1" type="submit"
-                    onClick={this.addNewUser(
-                        document.getElementById("Email"),
-                        document.getElementById("Username"),
-                        document.getElementById("Password"),
-                        this.getChecksLiked(),
-                        this.getChecksDisliked(),
-                        document.getElementById("Liked_Movies"),
-                        document.getElementById("Disliked_Movies"))}>Sign up</button>
+            <button class="btn btn-sm btn-block text-white text-center  btn-outline-light col-sm-1"
+                    onClick={this.addNewUser}>Sign up</button>
             </div>
         </body>);
     }
